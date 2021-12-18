73b45b37d9005eefd6e54a8ed61d7a50038d62e7
jungwolf <75339254+jungwolf@users.noreply.github.com>
2021-12-17 21:13:08 -0600

    Update part2_solution.sql

--- Day17/part2_solution.sql
+++ Day17/part2_solution.sql
@@ -1,3 +1,11 @@
+/*
+target area: x=277..318, y=-92..-53
+generate the candidates.
+For various reasons while working through part 1, I named the initial velocities ivx and ivy.
+The maximum legal initial x velocity hits the edge of the box at 318. I'm pretty sure I can find the minimum value that can reach 277 but I'm happy with the DB figuring that out.
+The maximum legal initial y velocity also hits the farthest edge of the box. Up mirrors down so I'll just run through the full range, abs(-92) down to -92.
+At most, ivy 92 will take 92 steps up, 92 back to y=0, and then vy=-93 so next goes past the box. t up to 318 is overkill.
+*/
 create or replace view day17_part2_v1 as
 with x as (SELECT ROWNUM x FROM ALL_OBJECTS WHERE ROWNUM <= 318)
 , y as (SELECT ROWNUM-92-1 y FROM ALL_OBJECTS WHERE ROWNUM <= (92+92+1))
