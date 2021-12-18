a4ca8209cc70f6cd90238b77c1ff3a94c4e6c287
jungwolf <75339254+jungwolf@users.noreply.github.com>
2021-12-17 20:56:57 -0600

    Update Part2_workbook.sql

--- Day17/Part2_workbook.sql
+++ Day17/Part2_workbook.sql
@@ -136,7 +136,14 @@ where 1=1
 /
 -- 9607, too high
 
+
 -- it really really helps if you read the problem text correctly
+create or replace view day17_part2_v1 as
+with x as (SELECT ROWNUM x FROM ALL_OBJECTS WHERE ROWNUM <= 318)
+, y as (SELECT ROWNUM-92-1 y FROM ALL_OBJECTS WHERE ROWNUM <= (92+92+1))
+, t as (SELECT ROWNUM t FROM ALL_OBJECTS WHERE ROWNUM <= 318)
+select x ivx, y ivy,t from x,y,t;
+
 select count(*) from (
 select unique ivx, ivy
 from day17_part2_v1
