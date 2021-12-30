ada1e79b08d59f5fe28b5f55915be264d6040421
jungwolf <75339254+jungwolf@users.noreply.github.com>
2021-12-30 03:32:07 -0500

    Create Part2_worksheet.sql

---
+++ Day09/Part2_worksheet.sql
@@ -0,0 +1,11 @@
+/*
+-- basins from example
+
+..999.....
+.9...9.9..
+9.....9.9.
+.....9...9
+9.999.....
+
+Find the three largest basins and multiply their sizes together. In the above example, this is 9 * 14 * 9 = 1134.
+*/
