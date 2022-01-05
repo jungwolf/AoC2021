eb047ad6a6e6e262f8ff63e12d19f207f5d5bcc9
jungwolf <75339254+jungwolf@users.noreply.github.com>
2022-01-05 12:01:10 -0600

    Update part2_worksheet.sql

--- Day12/part2_worksheet.sql
+++ Day12/part2_worksheet.sql
@@ -60,3 +60,40 @@ where node2 = 'end'
 order by current_path;
 
 -- probably needd something like case(maxvisits,2,2,1,regx...,0,regx...)...
+
+-- things got a little nutty
+-- 122134, taking 1m 34s.
+select count(*) from (
+with t (node1,node2,lvl,current_path,numvisits,maxvisits) as (
+  select node1, node2, 1 lvl
+--    , node1||','||node2 current_path
+,case when node2=lower(node2) then node1||','||node2 else node1 end current_path
+    , 0 numvisits, 0 maxvisits
+  from day12_part1_v2
+  where node1='start'
+
+  union all
+
+  select
+    v2.node1
+    ,v2.node2
+    ,t.lvl+1
+--    ,t.current_path||','||v2.node2
+, case when v2.node2=lower(v2.node2) then t.current_path||','||v2.node2 else t.current_path end
+--    ,regexp_count(t.current_path||','||v2.node2,lower(v2.node2)) numvisits
+, case when v2.node2=lower(v2.node2) then regexp_count(t.current_path||','||v2.node2,lower(v2.node2)) else 0 end
+    ,decode(t.maxvisits,2,2,t.numvisits) maxvisits
+  from t, day12_part1_v2 v2
+  where 1=1
+    and t.node2=v2.node1
+    and lvl = lvl
+--    and lvl < 15
+--  and lvl < 11000 -- prevent runaway...
+    and ( (t.numvisits < 2) or (t.numvisits = 2 and t.maxvisits < 2) )
+)
+select * from t
+where node2 = 'end'
+--where lvl=9
+)
+/
+-- 122134
